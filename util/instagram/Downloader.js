867fb83944469a1c26b3aba928c8e1cb6e4726b5
fauzanfebrian <73564532+fauzanfebrian@users.noreply.github.com>
2021-03-14 16:05:49 +0700

    Update Downloader.js

--- util/instagram/Downloader.js
+++ util/instagram/Downloader.js
@@ -1,6 +1,15 @@
 const instagram_download = require("@juliendu11/instagram-downloader");
+const axios = require("axios");
 (async () => {})();
 module.exports.Downloader = async (url) => {
   const value = await instagram_download.downloadMetaData(url);
+  axios.get('url)
+  .then((response) => {
+    console.log("GET Response")
+    console.log(response.data.graphql);
+  })
+  .catch(function (error) {
+    console.log("Error in fetching market updates");
+  });  
   return value;
 };
