d94991d0f554df30bb813fc43c29b7a892f952e3
fauzanfebrian <fauzanfebrian1725@gmail.com>
2021-04-05 15:44:54 +0700

    bismillah

--- web/index.php
+++ web/index.php
@@ -14,31 +14,47 @@
 
 require"api.php";
 
-$ua = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
-ini_set("user_agent", $ua);
+require('../vendor/autoload.php');
 
-$url = isset($_GET['url']) ? $_GET['url'] : "";
+$app = new Silex\Application();
+$app['debug'] = true;
 
-if (!empty($url)) {
-    if (checkUrl($url)) {
-        header('Content-Type:application/json');
-        echo genLink(file_get_contents($url));
-        exit;
+// Register the monolog logging service
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+  'monolog.logfile' => 'php://stderr',
+));
+
+$app->get('/', function() use($app) {
+    $ua = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+    ini_set("user_agent", $ua);
+
+    $url = isset($_GET['url']) ? $_GET['url'] : "";
+
+    if (!empty($url)) {
+        if (checkUrl($url)) {
+            header('Content-Type:application/json');
+            echo genLink(file_get_contents($url));
+            exit;
+        } else {
+            header('Content-Type:application/json');
+            $out = array(
+                    "status"  => "error",
+                    "err_msg" => "invalid instagram url!",
+                );
+            echo json_encode($out, JSON_PRETTY_PRINT);
+            exit;
+        }
     } else {
         header('Content-Type:application/json');
         $out = array(
                 "status"  => "error",
-                "err_msg" => "invalid instagram url!",
-               );
+                "err_msg" => "no input!",
+            );
         echo json_encode($out, JSON_PRETTY_PRINT);
         exit;
-    }
-} else {
-    header('Content-Type:application/json');
-    $out = array(
-            "status"  => "error",
-            "err_msg" => "no input!",
-           );
-    echo json_encode($out, JSON_PRETTY_PRINT);
-    exit;
-}//end if
+    }//end if
+});
+  
+$app->run();
+  
+
